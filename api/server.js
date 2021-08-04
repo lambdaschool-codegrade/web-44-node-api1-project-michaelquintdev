52af69fa49560760011bf6a30541715de6605378
michaelquintdev <michaelquintdev@gmail.com>
2021-08-03 20:56:20 -0500

    Done with project

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 // BUILD YOUR SERVER HERE
+const { json } = require('express')
 const express = require('express')
 const User = require('./users/model')
 const server = express()
@@ -50,6 +51,28 @@ server.post('/api/users/', (req, res) => {
                 err: err.message,
                 stack: err.stack
             })
+        })
+    }
+})
+
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if(!possibleUser) {
+            res.status(404).json({message: 'message: "The user with the specified ID does not exist'})
+        } else {
+            if(!req.body.name || !req.body.bio){
+                res.status(400).json({message: 'Please provide name and bio for the user'})
+            }else{
+                const updatedUser = await User.update(req.params.id, req.body)
+                res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error getting users',
+            err: err.message,
+            stack: err.stack
         })
     }
 })
